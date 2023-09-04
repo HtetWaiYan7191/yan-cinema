6289c2b941da3ccc176f434bf59c88fbadd718f9
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-04 19:53:56 +0630

    Set up react router in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Detail from './pages/Detail'
+import Search from './pages/Search'
 
 const App = () => {
   return (
-    <div className=' text-red-400'>
-      This is app 
-    </div>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/detail/:productId' element={<Detail/>}/>
+    <Route path='/search/:movie' element={<Search/>}/>
+  </Routes>
   )
+ 
 }
 
 export default App
