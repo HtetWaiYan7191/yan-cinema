c3d2f8855f4e62b70e95b7f9de5bf1ff82a66458
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-05 21:04:42 +0630

    Add new movies and series pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <section className='home-wrapper py-32' id='home-section'>
       <h2 className=' text-red-500 text-2xl text-center'>Home</h2>
-    </div>
+    </section>
   )
 }
 
