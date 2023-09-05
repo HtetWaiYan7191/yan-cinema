2eb5244265dd6c4ad319d25c1d6fd44d74bc7f02
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-05 21:05:06 +0630

    Implement navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
 import Search from './pages/Search'
+import Movies from './pages/Movies'
+import Series from './pages/Series'
 
 const App = () => {
   return (
     <Routes>
     <Route path='/' element={<Home/>}/>
+    <Route path='/movies' element={<Movies/>}></Route>
+    <Route path='/series' element={<Series/>}></Route>
     <Route path='/detail/:productId' element={<Detail/>}/>
     <Route path='/search/:movie' element={<Search/>}/>
   </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../styles/Navbar.css'
+
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <section className='navbar-section flex justify-center bg-black/90 text-white fixed w-[100%] top-0'>
+      <div className='navbaar-container flex items-center justify-between w-[80%] py-5'>
+        <h2 className='text-3xl text-red-600 font-semibold'>Yan Cinema</h2>
+        <ul className='navbar-list flex'>
+          <NavLink to='/' className='me-4 font-semibold'><li className=' '>Home</li></NavLink>
+          <NavLink to='/movies' className='me-4 font-semibold'><li className=''>Movies</li></NavLink>
+          <NavLink to='/series' className='me-4 font-semibold'><li className=''>TV Series</li></NavLink>
+        </ul>
+      </div>
+    </section>
   )
 }
 
