4d247c300217ceffd9c924134c16c71e720f6876
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-07 22:09:25 +0630

    Add slice for movie , fetch api

--- src/redux/movieSlice.js
+++ src/redux/movieSlice.js
@@ -5,11 +5,28 @@ const initialState = {
     error : false,
     value: []
 }
+const MOVIEAPI = 'https://api.tvmaze.com/show';
+export const fetchMovies = createAsyncThunk('movies/fetchMovies', async () => {
+    console.log("fetching api")
+    const response = await fetch(MOVIEAPI)
+    if(!response.ok) {
+        throw new Error("Fail to fetch movies")
+    }
+    const data = await response.json();
+    return data;
+})
 
 const movieSlice = createSlice({
     name: 'movie',
     initialState,
-    reducers:{}
+    extraReducers: (builder) => {
+        builder.addCase(fetchMovies.fulfilled, (state, action) => {
+            state.loading = false;
+            state.value = action.payload;
+            state.error = "";
+        })
+    }
+    
 })
 
 export default movieSlice.reducer
