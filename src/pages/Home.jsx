d2ce69ff2ad7fd1dbfd1564638e189d65a6737c3
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-07 22:09:45 +0630

    Implement custom styles for carousel buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import HeroSlide from '../components/HeroSlide'
 import MovieList from '../components/MovieList'
+import {useSelector, useDispatch} from 'react-redux'
+import { fetchMovies } from '../redux/movieSlice'
 
 const Home = () => {
+  const movies = useSelector((state) => state.movie.value);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    if(movies.length === 0 ) {
+      dispatch(fetchMovies())
+    }
+  }, [movies])
+
   return (
     <section className='home-wrapper bg-black' id='home-section'>
       <HeroSlide/>
-      <MovieList/>
+      <MovieList movies = {movies}/>
     </section>
   )
 }
