425e0eaa2250bc1e38558668aa0c585724629321
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-13 22:41:12 +0630

    Add movie card component for each movie

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import '../styles/MovieList.css';
+
+const MovieCard = ({ category, movies }) => {
+console.log(movies)
+  return (
+    <div className="movie-card w-[80%] h-[50vh] mx-auto">
+      <h2 className="text-white text-3xl font-semibold mb-6">{category}</h2>
+      <Swiper 
+       modules={[Navigation, Pagination, Scrollbar, A11y]}
+       spaceBetween={25}
+       slidesPerView={7}
+       navigation
+       scrollbar={{ draggable: true }}>
+      {movies.map((movie) => (
+        <SwiperSlide key={movie.id}>
+          <figure className=" overflow-hidden w-[100%]">
+            <img
+              src={movie.image.medium}
+              alt=""
+              className="rounded-lg overflow-hidden w-[100%]  object-cover"
+            />
+          </figure>
+        </SwiperSlide>
+      ))}
+         </Swiper>
+    </div>
+  );
+};
+
+export default MovieCard;
