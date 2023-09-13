1183ed105bb928320d170419765150245604ba57
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-13 21:06:28 +0630

    Update redux store

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,21 @@ import MovieList from '../components/MovieList'
 import Trending from '../components/Trending'
 import {useSelector, useDispatch} from 'react-redux'
 import { fetchMovies } from '../redux/movieSlice'
+import { fetchTrending } from '../redux/trendingSlice'
 
 const Home = () => {
   const movies = useSelector((state) => state.movie.value);
+  const trending = useSelector((state) => state.trending.value);
+  const categories = useSelector((state) => state.movie.categories);
+  console.log(categories)
   const dispatch = useDispatch();
   
   useEffect(() => {
-    if(movies.length === 0 ) {
+    if(movies.length === 0 && trending.length === 0 ) {
       dispatch(fetchMovies())
+      dispatch(fetchTrending())
     }
-  }, [movies])
+  }, [movies, trending])
 
   return (
     <section className='home-wrapper bg-black' id='home-section'>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import detailSliceReducer from './detailSlice'
 import movieSliceReducer from './movieSlice'
+import trendingSliceReducer from './trendingSlice'
 
 const store = configureStore({
     reducer: {
         detail: detailSliceReducer,
         movie: movieSliceReducer,
+        trending: trendingSliceReducer,
     }
 })
 
