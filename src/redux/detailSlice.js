0ff092e06cce5a7f38a1f8441350ac7913cf6219
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-14 21:52:50 +0630

    Add detailSlice to manage detail movie data

--- src/redux/detailSlice.js
+++ src/redux/detailSlice.js
@@ -2,15 +2,44 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
     loading: false,
-    error: '',
-    value: []
+    error : '',
+    value: {},
 }
+
+const BASEAPI = ' https://api.tvmaze.com/shows'
+export const fetchDetailMovie = createAsyncThunk('detail/fetchDetailMovie', async (id) => {
+    const response = await fetch(`${BASEAPI}/${id}`)
+    if(!response.ok) {
+        throw new Error("Fail to fetch movies")
+    }
+    const data = await response.json();
+    return data;
+})
+
 
 const detailSlice = createSlice({
     name: 'detail',
     initialState,
-    reducers: {},
-    
+    reducers:{},
+    extraReducers: (builder) => {
+        builder.addCase(fetchDetailMovie.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = '';
+            state.value = {
+                id: action.payload.id,
+                name: action.payload.name,
+                category: action.payload.genres,
+                status: action.payload.status,
+                premiered: action.payload.premiered,
+                ended: action.payload.ended,
+                officialSite: action.payload.officialSite,
+                rating: action.payload.rating,
+                image: action.payload.image,
+                summary: action.payload.summary,
+            }
+        })
+    }
+
 })
 
 export default detailSlice.reducer;
