f31b5510103c1d6c929cde29102b697a2e3328ac
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-18 23:47:22 +0630

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import detailSliceReducer from './detailSlice'
 import movieSliceReducer from './movieSlice'
 import trendingSliceReducer from './trendingSlice'
+import castSliceReducer from './castSlice'
 
 const store = configureStore({
     reducer: {
         detail: detailSliceReducer,
         movie: movieSliceReducer,
         trending: trendingSliceReducer,
+        cast: castSliceReducer,
     }
 })
 
