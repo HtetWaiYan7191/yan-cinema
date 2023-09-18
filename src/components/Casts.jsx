673d4eddb04d1ef5c93030f1f1309682a99a30f6
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-18 23:47:46 +0630

    Show casts images and names in casts component

--- src/components/Casts.jsx
+++ src/components/Casts.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import '../styles/Casts.css';
 
-const Casts = () => {
+const Casts = ({ casts }) => {
   return (
-    <div>
-      casts
+    <div className="w-[80%] mx-auto mt-10 ps-4 grid grid-cols-2 md:grid-cols-5 gap-10">
+      {casts.map((cast) => (
+        <div className="cast-card" key={cast.id}>
+          <img
+            src={cast.image}
+            alt=""
+            className="rounded-md overflow-hidden cast-img"
+          />
+          <button className="mt-5 cast-button font-semibold text-lg p-2 w-[100%] rounded-sm text-center hover:text-red-500 hover:border-rose-400 text-white border">
+            {cast.name}
+          </button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Casts
+export default Casts;
